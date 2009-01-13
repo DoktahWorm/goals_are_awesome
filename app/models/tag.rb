ea790be771fe51326c82b836e05cebe33122bbef
Kyle Oba <koba@mudphone.com>
2009-01-13 14:01:16 +0100

    Added find_popular method to Tag model.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,4 +3,13 @@ class Tag < ActiveRecord::Base
       :from => [:goals], 
       :through => :taggings,
       :dependent => :destroy
+  
+  def self.find_popular(args = {})
+    find(:all, :select => 'tags.*, count(*) as popularity', 
+      :limit => args[:limit] || 10,
+      :joins => "JOIN taggings ON taggings.tag_id = tags.id",
+      :conditions => args[:conditions],
+      :group => "taggings.tag_id", 
+      :order => "popularity DESC"  )
+  end
 end
